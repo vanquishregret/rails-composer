a9bd202e85c8862cfea806cab1b3e3f862965b79
Dave <mersilde@gmail.com>
2020-02-07 12:51:16 +0300

    Update composer.rb

--- composer.rb
+++ composer.rb
@@ -355,6 +355,30 @@ if options[:verbose]
 end
 
 case Rails::VERSION::MAJOR.to_s
+when "6"
+  prefs[:apps4] = multiple_choice "Build a starter application?",
+    [["Build a RailsApps example application", "railsapps"],
+    ["Contributed applications", "contributed_app"],
+    ["Custom application (experimental)", "none"]] unless prefs.has_key? :apps4
+  case prefs[:apps4]
+    when 'railsapps'
+        prefs[:apps4] = multiple_choice "Choose a starter application.",
+        [["learn-rails", "learn-rails"],
+        ["rails-bootstrap", "rails-bootstrap"],
+        ["rails-foundation", "rails-foundation"],
+        ["rails-mailinglist-activejob", "rails-mailinglist-activejob"],
+        ["rails-omniauth", "rails-omniauth"],
+        ["rails-devise", "rails-devise"],
+        ["rails-devise-roles", "rails-devise-roles"],
+        ["rails-devise-pundit", "rails-devise-pundit"],
+        ["rails-signup-download", "rails-signup-download"],
+        ["rails-stripe-checkout", "rails-stripe-checkout"],
+        ["rails-stripe-coupons", "rails-stripe-coupons"]]
+    when 'contributed_app'
+      prefs[:apps4] = multiple_choice "Choose a starter application.",
+        [["rails-shortcut-app", "rails-shortcut-app"],
+        ["rails-signup-thankyou", "rails-signup-thankyou"]]
+  end
 when "5"
   prefs[:apps4] = multiple_choice "Build a starter application?",
     [["Build a RailsApps example application", "railsapps"],
